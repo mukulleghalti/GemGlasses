17102b199893fe6534a797d11192bd823b2ddabf
mukulleghalti <82515213+mukulleghalti@users.noreply.github.com>
2026-09-20 03:20:35 +0000

    finalee

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,26 @@ dependencyResolutionManagement {
             repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
                 repositories {
                                 google()
-                                        mavenCentral() // The Meta SDK is now here!
+                                        mavenCentral()
+                                                
+                                                        val localProps = java.util.Properties()
+                                                                val f = rootDir.resolve("local.properties")
+                                                                        if (f.exists()) f.inputStream().use { localProps.load(it) }
+
+                                                                                val githubToken = localProps.getProperty("github_token") ?: System.getenv("GITHUB_TOKEN")
+                                                                                        val githubActor = localProps.getProperty("github_actor") ?: System.getenv("GITHUB_ACTOR")
+
+                                                                                                if (!githubToken.isNullOrBlank()) {
+                                                                                                                    maven {
+                                                                                                                                        name = "MetaWearables"
+                                                                                                                                                        // Using the exact URL confirmed by your curl success
+                                                                                                                                                                        url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
+                                                                                                                                                                                        credentials {
+                                                                                                                                                                                                                    username = githubActor ?: "gemglasses"
+                                                                                                                                                                                                                                        password = githubToken
+                                                                                                                                                                                        }
+                                                                                                                    }
+                                                                                                }
                 }
 }
 
