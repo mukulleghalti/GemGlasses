8ea8a0f1ddbf5a9217360b635ccf4c932b55d5b4
mukulleghalti <82515213+mukulleghalti@users.noreply.github.com>
2026-09-20 09:01:38 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,37 +1,23 @@
 pluginManagement {
-            repositories {
-                        google()
-                                mavenCentral()
-                                        gradlePluginPortal()
-            }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
-
 dependencyResolutionManagement {
-            repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-                repositories {
-                                google()
-                                        mavenCentral()
-                                                
-                                                        val localProps = java.util.Properties()
-                                                                val f = rootDir.resolve("local.properties")
-                                                                        if (f.exists()) f.inputStream().use { localProps.load(it) }
-
-                                                                                val githubToken = localProps.getProperty("github_token") ?: System.getenv("GITHUB_TOKEN")
-                                                                                        val githubActor = localProps.getProperty("github_actor") ?: System.getenv("GITHUB_ACTOR")
-
-                                                                                                if (!githubToken.isNullOrBlank()) {
-                                                                                                                    maven {
-                                                                                                                                        name = "MetaWearables"
-                                                                                                                                                        // Using the exact URL confirmed by your curl success
-                                                                                                                                                                        url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
-                                                                                                                                                                                        credentials {
-                                                                                                                                                                                                                    username = githubActor ?: "gemglasses"
-                                                                                                                                                                                                                                        password = githubToken
-                                                                                                                                                                                        }
-                                                                                                                    }
-                                                                                                }
-                }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: "gemglasses"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
-
 rootProject.name = "GemGlasses"
 include(":app")
